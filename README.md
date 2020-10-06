439c29ef4ee9f90eda24968271a7589332ba868d
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-10-06 12:33:26 -0400

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# questiongenerator
+
+The following animation demonstrates the application functionality:
+
+
+Create a quiz. 
+*The user will read each question and click on the answer they believe is correct. 
+
+*There will be a timer that gives them a designated amount of time to complete the quiz. 
+
+*When the user responds to each question, the browser will inform them if their response was correct or incorrect. 
+
+*When the user selects an incorrect answer, the score decrements by 1, and two penalty seconds are taken off of the timer.  When the user selects a correct answer, the score increments by 1 and the quiz continues.
+
+*There is a two-second interval between each question. 
+
+*When the timer runs out of time, the quiz is over.  If the user finishes all of the questions before time runs out, the quiz is over.
+
+*When the quiz is over, the user’s name and score will be displayed, along with a high score button where a list of high scores are stored.
+
+link to deployed application: https://kaitlinkenney.github.io/questiongenerator/
+
+screenshot:
--- script.js
+++ script.js
@@ -5,11 +5,35 @@ var questions = [
       answer: "alerts",
     },
     {
-      question:
-        "The condition in an if / else statement is enclosed within ____.",
+      question: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses",
     },
+  //   {
+  //     question: "What is the biggest island in the world?",
+  //     choices: ["Greenland", "Antarctica", "Australia", "New Zealand"],
+  //     answer: "Greenland",
+  //   },
+  //   {
+  //     question: "Who saved James Potter's life in the Harry Potter series?",
+  //     choices: ["Dumbledore", "Ron Weasley", "Snape", "Sirius Black"],
+  //     answer: "Snape",
+  //   },
+  //   {
+  //     question: "What is a good topping for beef tacos?",
+  //     choices: ["ketchup", "Adobo", "lemon pepper", "mayonnaise"],
+  //     answer: "Adobo",
+  //   }
+  //   {
+  //     question: "In Breaking Bad, what subject did Walter White teach?",
+  //     choices: ["chemistry", "biology", "history", "French"],
+  //     answer: "chemistry",
+  //   },
+  //   {
+  //     question: "Which of the following is an anime film?",
+  //     choices: ["Family Guy", "Attack on Titan", "Bob's Burgers", "Vampire Diaries"],
+  //     answer: "Attack on Titan",
+  //   }
   ];
   
   var questionEl = document.querySelector("#question");
@@ -17,21 +41,25 @@ var questions = [
   var questionResultEl = document.querySelector("#question-result");
   var timerEl = document.querySelector("#timer");
   var highscoresbtn = document.querySelector("#highscoresbtn");
-  
-  
+
+
   var questionIndex = 0;
   var correctCount = 0;
- 
- 
+
+
   var time = 20;
   var intervalId;
-  
+
   function endQuiz() {
     clearInterval(intervalId);
     var body = document.body;
     body.innerHTML = "Game over, You scored " + correctCount;
-
     
+    highscoresbtn.addEventListener("click", showHighScore);
+      clearInterval(intervalId);
+      document.body.appendChild(highscoresbtn);
+
+    setTimeout (showHighScore, 2);
   }
 
 
@@ -39,71 +67,69 @@ var questions = [
 
     var name = prompt("Please enter your name");
 
-  
+
     var high_scores = localStorage.getItem("scores");
-  
+
     if (!high_scores) {
       high_scores = [];
     } else {
       high_scores = JSON.parse(high_scores);
     }
-  
+
     high_scores.push({ name: name, score: correctCount});
-  
+
     localStorage.setItem("scores", JSON.stringify(high_scores));
-  
+
     high_scores.sort(function (a, b) {
       return b.score - a.score;
     });
 
-    highscoresbtn.addEventListener("click", showHighScore);
-    document.body.appendChild(highscoresbtn);
-  
-  setTimeout (showHighScore, 2);
-  
+    
+        
+
     var contentUL = document.createElement("ul");
-  
+
     for (var i=0; i < high_scores.length; i++) {
       var contentLI = document.createElement("li");
       contentLI.textContent =
         "Name: " + high_scores[i].name + " Score: " + high_scores[i].score;
       contentUL.appendChild(contentLI);
     }
-  
+
   document.body.appendChild(contentUL);
   }
-  
+
   function updateTime() {
     time--;
-    timerEl.textContent = time;
+    timerEl.textContent = "Timer:  " + time;
     if (time <= 0) {
       endQuiz();
     }
   }
-  
+
   function renderQuestion() {
-  
+
     if (time == 0) {
       updateTime();
       return;
     }
-    
+
     intervalId = setInterval(updateTime, 1000);
     questionEl.textContent = questions[questionIndex].question;
-  
+
     optionListEl.innerHTML = "";
     questionResultEl.innerHTML = "";
-  
+
     var choices = questions[questionIndex].choices;
     var choicesLength = choices.length;
-  
+
     for (var i = 0; i < choicesLength; i++) {
       var questionListItem = document.createElement("li");
       questionListItem.textContent = choices[i];
       optionListEl.append(questionListItem);
     }
   }
-  
+
   function nextQuestion() {
     questionIndex++;
     if (questionIndex === questions.length) {
@@ -111,7 +137,7 @@ var questions = [
     }
     renderQuestion();
   }
-  
+
   function checkAnswer(event) {
     clearInterval(intervalId);
     if (event.target.matches("li")) {
@@ -127,7 +153,6 @@ var questions = [
     }
     setTimeout(nextQuestion, 2000);
   }
-  
+
   renderQuestion();
   optionListEl.addEventListener("click", checkAnswer);
-  
